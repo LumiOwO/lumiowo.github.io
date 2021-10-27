12ee27de1eba7aedec0ede73617aef13d7563245
LumiOwO <948697775@qq.com>
2021-10-27 18:07:28 +0800

    add copy button to code block

---
+++ themes/yilia/source/js/clipboard_use.js
@@ -0,0 +1,37 @@
+$(".highlight").wrap("<div class='code-wrapper' style='position:relative'></div>");
+/*页面载入完成后，创建复制按钮*/
+!function (e, t, a) {
+    /* code */
+    var initCopyCode = function () {
+        var copyHtml = '';
+        copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+        copyHtml += '  <i class="fa fa-clipboard">'; // copy
+        copyHtml += '</button>';
+        $(".highlight .code").before(copyHtml);
+        var clipboard = new ClipboardJS('.btn-copy', {
+            target: function (trigger) {
+                return trigger.nextElementSibling;
+            }
+        });
+        clipboard.on('success', function (e) {
+          e.trigger.innerHTML =
+            "<span style='color: #0f0;'><i class='fa fa-check' color='#0f0'></i></span>"; // copy success
+          setTimeout(function () {
+            e.trigger.innerHTML =
+              "<i class='fa fa-clipboard'>"; // copy
+          }, 1000);
+
+          e.clearSelection();
+        });
+        clipboard.on('error', function (e) {
+          e.trigger.innerHTML =
+            "<span style='color: #f00;'><i class='fa fa-exclamation-circle'></i></span>"; // copy error
+          setTimeout(function () {
+            e.trigger.innerHTML =
+              "<i class='fa fa-clipboard'>"; // copy
+          }, 1000);
+          e.clearSelection();
+        });
+    }
+    initCopyCode();
+}(window, document);
